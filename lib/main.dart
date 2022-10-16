b122a5ca11b8c0cecaf7f19cec4afe5a16da6efb
muradhossin <murad15-2547@diu.edu.bd>
2022-10-16 14:09:13 +0600

    basic provider setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
+import 'dart:js';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:simple_calculator/pages/home_page.dart';
+import 'package:simple_calculator/provider/calculator_provider.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: HomePage.routeName,
-    routes: {
-      HomePage.routeName: (context) => HomePage(),
-    },
+  runApp(ChangeNotifierProvider(
+    create: (context) => CalculatorProvider(),
+    child: const myApp(),
   ));
+}
+
+class myApp extends StatelessWidget {
+  const myApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomePage.routeName,
+      routes: {
+        HomePage.routeName: (context) => HomePage(),
+      },
+    );
+  }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_calculator/provider/calculator_provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,6 +8,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final provider = Provider.of<CalculatorProvider>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Simple Calculator'),
